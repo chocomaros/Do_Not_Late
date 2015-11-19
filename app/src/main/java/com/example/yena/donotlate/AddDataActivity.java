1fe5901f1f3c2c3a2c53175f21ecbcf44d4f164e
yena <yena@example.com>
2015-11-19 19:18:50 +0900

    정보 추가 액티비티 장소선택이랑 확인 버튼 만들어야함!

--- app/src/main/java/com/example/yena/donotlate/AddDataActivity.java
+++ app/src/main/java/com/example/yena/donotlate/AddDataActivity.java
@@ -11,6 +11,7 @@ import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -19,49 +20,45 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class AddDataActivity extends Activity {
-
+    TextView tvTime,tvDate;
+    EditText etTitle;
     ListData data = new ListData("",new Day());
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_data);
-        Log.d("ㅁㄴㅇㄹ","안되나?");
-        Log.d("날짜", " " + data.dDay.year + "년" + data.dDay.month + "월" + data.dDay.day + "일");
+        tvTime = (TextView) findViewById(R.id.tv_insert_time);
+        tvDate =(TextView) findViewById(R.id.tv_insert_date);
 
-        findViewById(R.id.tv_insert_date).setOnClickListener(new View.OnClickListener() {
+        tvDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Calendar calendar = Calendar.getInstance();
                 DatePickerDialog dialog = new DatePickerDialog((new ContextThemeWrapper((AddDataActivity.this),
                         android.R.style.Theme_Holo_Light_Dialog_NoActionBar)), listener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE));
-                Log.d("날짜"," "+data.dDay.year+"년"+data.dDay.month+"월"+data.dDay.day+"일");
                 dialog.show();
-                Log.d("날짜"," "+data.dDay.year+"년"+data.dDay.month+"월"+data.dDay.day+"일");
             }
         });
 
-        findViewById(R.id.tv_insert_time).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar calendar = Calendar.getInstance();
-                TimePickerDialog timePicker = new TimePickerDialog((new ContextThemeWrapper(AddDataActivity.this,
-                        android.R.style.Theme_Holo_Light_Dialog_NoActionBar)),myTimeSetListener,calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),true);
-                timePicker.show();
-            }
-        });
-
+       tvTime.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Calendar calendar = Calendar.getInstance();
+               TimePickerDialog timePicker = new TimePickerDialog((new ContextThemeWrapper(AddDataActivity.this,
+                       android.R.style.Theme_Holo_Light_Dialog_NoActionBar)), myTimeSetListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true);
+               timePicker.show();
+           }
+       });
 
     }
 
     private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
         @Override
         public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-            AddDataActivity.this.data.dDay.year = year;
-            AddDataActivity.this.data.dDay.month = monthOfYear;
-            AddDataActivity.this.data.dDay.day = dayOfMonth;
-            Toast.makeText(getApplicationContext(), year + "년" + monthOfYear + "월" + dayOfMonth + "일", Toast.LENGTH_SHORT).show();
-            Log.d("날짜", " " + data.dDay.year + "년" + data.dDay.month + "월" + data.dDay.day + "일");
+            data.dDay.year = year;
+            data.dDay.month = monthOfYear;
+            data.dDay.day = dayOfMonth;
+            tvDate.setText(" "+year+". "+(monthOfYear+1)+". "+dayOfMonth);
         }
     };
 
@@ -69,9 +66,14 @@ public class AddDataActivity extends Activity {
             = new TimePickerDialog.OnTimeSetListener() {
 
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            String time = "Hour: " + String.valueOf(hourOfDay) + "\n"
-                    + "Minute: " + String.valueOf(minute);
-            Toast.makeText(getApplicationContext(), time, Toast.LENGTH_LONG).show();
+            data.dDay.hour = hourOfDay;
+            data.dDay.minute = minute;
+            if(minute < 10){
+                tvTime.setText(" "+hourOfDay+": 0"+minute);
+            }
+            else {
+                tvTime.setText(" " + hourOfDay + ": " + minute);
+            }
         }
     };
 
