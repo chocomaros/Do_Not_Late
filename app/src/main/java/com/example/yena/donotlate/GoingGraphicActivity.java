760983072ee886ce57ab31f21cbabb66551e2ea3
yena <yena@example.com>
2015-11-27 01:29:48 +0900

    이미지가 안움직인다... ㅠㅠ

---
+++ app/src/main/java/com/example/yena/donotlate/GoingGraphicActivity.java
@@ -0,0 +1,62 @@
+package com.example.yena.donotlate;
+
+import android.graphics.Matrix;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GoingGraphicActivity extends AppCompatActivity {
+
+    ImageView ivBird;
+    ProgressBar progressBar;
+    int percent = 40;
+    ListData data;
+    Matrix matrixImage = new Matrix();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_going_graphic);
+        ivBird = (ImageView) findViewById(R.id.bird_image);
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setProgress(percent);
+        ArrayList<ListData> appointmentList = YenaDAO.getCurrentList(getApplicationContext());
+        data = appointmentList.get(0);
+        setImagePosition();
+        ivBird.setScaleType(ImageView.ScaleType.MATRIX);
+        ivBird.setImageMatrix(matrixImage);
+    }
+
+    public void onWindowFocusChanged(boolean hasFocus) {
+        // TODO Auto-generated method stub
+        ivBird.setBackgroundResource(R.drawable.bird_animation);
+        AnimationDrawable frameAnimation = (AnimationDrawable) ivBird.getBackground();
+        frameAnimation.start();
+        setImagePosition();
+        ivBird.setScaleType(ImageView.ScaleType.MATRIX);
+        ivBird.setImageMatrix(matrixImage);
+        super.onWindowFocusChanged(hasFocus);
+    }
+
+//    void setImagePosition(){
+//        int[] location = new int[2]; /// left = location[0], top = location[1]
+//        progressBar.getLocationOnScreen(location);
+//
+//        int right = location[0] + progressBar.getWidth();
+//        int bottom = location[1] + progressBar.getHeight();
+//
+//        imageLocation[0] = ((right + location[0]) / 100)* percent;
+//        imageLocation[1] = location[1] + 20;
+//    }
+
+    void setImagePosition(){
+        matrixImage.postTranslate(progressBar.getScrollX(),100);
+    }
+
+}
--- app/src/main/java/com/example/yena/donotlate/StartActivity.java
+++ app/src/main/java/com/example/yena/donotlate/StartActivity.java
@@ -7,26 +7,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
 public class StartActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        if (!isStarted()) {
+            setContentView(R.layout.activity_start);
 
-        Button showList = (Button)findViewById(R.id.bt_list);
+            Button showList = (Button) findViewById(R.id.bt_list);
 
-        ListDataDBHelper mDBHelper = new ListDataDBHelper(getApplicationContext());
+            showList.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(StartActivity.this, GoingGraphicActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }else{
+            Intent intent = new Intent(StartActivity.this, GoingGraphicActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
-//        mDBHelper.open().mDB.execSQL("insert into " + ListData.TABLE_NAME + " values (null, '으어어','adf'," +
-//                    "'연구실임','2015-11-24 03:33','2015-11-24 05:00', 1.13, 2.13, 3.3, 4.1, 1, 1 , 0)");
-
-        showList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(StartActivity.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
+    Boolean isStarted(){
+        ArrayList<ListData> appointmentList = YenaDAO.getCurrentList(getApplicationContext());
+        if(appointmentList.get(0).isStarted){
+            return true;
+        }else return false;
     }
 }
